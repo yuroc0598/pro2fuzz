6dfd8dc85e407c84b4782ff9315885861431048f
yuroc5390 <gabrielchen@gwu.edu>
2019-05-12 12:46:50 -0400

    multi runssl option

--- runssl.bash
+++ runssl.bash
@@ -1,4 +1,6 @@
-cd testcases/openssl && ../../afl-fuzz -i in -o out -m none -f input -t 400  -p 1 ./openssl
+version=$1
+bin=openssl$version
+cd testcases/$bin && ../../afl-fuzz -i in -o out -m none -f input -t 400  -p 1 ./$bin
 
 
 
